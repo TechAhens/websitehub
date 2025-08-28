0d88c7bcd8f6e89c0b2f4916f55b30ae0a557737
TechAhens <ahensproductions@gmail.com>
2025-08-28 13:27:20 +0530

    Updated webview.tsx

--- app/webview.tsx
+++ app/webview.tsx
@@ -86,7 +86,7 @@ export default function WebViewScreen() {
         >
           <ArrowLeft color="#374151" size={24} />
         </TouchableOpacity>
-          <View style={{ height: 4 }} />  {/* 👈 tiny spacer above text */}
+   
         <View style={styles.titleContainer}>
     <Text style={styles.title} numberOfLines={1}>
             {}
